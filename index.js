aad5e17733698d958cd2ef3c555fa6e5ce5f62a6
Samra Mammadli <memmedlisemra6@gmail.com>
2023-10-17 18:23:05 +0400

    GetValues from inputs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const button = document.querySelector('button');
 
+
+button.addEventListener('click',()=>{
+})
