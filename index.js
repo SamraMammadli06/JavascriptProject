5d123ba9ef54fc79a2c8f03396c6f0cb5dc23a65
Samra Mammadli <memmedlisemra6@gmail.com>
2023-10-18 18:02:23 +0400

    Add tasks to checkboxes

--- index.js
+++ index.js
@@ -8,12 +8,36 @@ const textarea = document.querySelector('textarea');
 const button = document.querySelector('button');
 const tasks = new Tasks();
 
+function CreateTaskList(){
+    const items = localStorage.getItem(TasksKey);
+    const task_items = JSON.parse(items);
+    
+    task_items.forEach(element => {
+        const checklist = document.createElement('input');
+        const label = document.createElement('label');
+        const br = document.createElement('br');
+        label.textContent = element.title + ' ';
+        checklist.type = 'checkbox';
+        label.append(checklist);
+        document.body.append(label);
+        document.body.append(br);
+
+        console.log(element);
+    });
+}
 
 button.addEventListener('click',()=>{
     let task = new Task(input.value,textarea.value);
     tasks.AddTask(task);
     const taskArray = tasks.Gettasks();
     localStorage.setItem(TasksKey,JSON.stringify(taskArray));
+
+    CreateTaskList();
+    
+
     input.value ='';
     textarea.value ='';
+
 })
+
+CreateTaskList();
